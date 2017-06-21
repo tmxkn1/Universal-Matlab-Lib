b978a16f8f4cda27852d8ef48c3cdf0312e19739
Home Desktop <anthony.zyjiang@outlook.com>
2017-06-22 00:06:12 +0100

    angle2d now works for array/matrix calculation.

--- other/angle2d.m
+++ other/angle2d.m
@@ -30,14 +30,25 @@ end
 v1 = varargin{1};
 v2 = varargin{2};
 
-v1 = v1/norm(v1);
-v2 = v2/norm(v2);
+s1 = size(v1);
+s2 = size(v2);
 
-a = atan2(v1(1)*v2(2) - v1(2)*v2(1), dot(v1,v2));
-
-if isnan(a)
-    a = 0;
+if ~isequal(s1,s2)
+    if isvector(v1)
+        v1 = repmat(v1,s2(1),1);
+    elseif isvecotr(v2)
+        v2 = repmat(v1,s1(1),1);
+    else
+        error('Input arguments should be two vectors, two matrices of the same size or one vector and one matrix')
+    end
 end
+
+v1 = v1./sqrt(sum(v1.^2,2));
+v2 = v2./sqrt(sum(v1.^2,2));
+
+a = atan2(v1(:,1).*v2(:,2) - v1(:,2).*v2(:,1), dot(v1',v2')');
+
+a(isnan(a)) = 0;
 
 if isdeg
     a = a*180/pi;
