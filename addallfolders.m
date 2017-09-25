d2ea5a3bc5f478af79fa1256b714b600ba68ae81
TONYSP3\Zhengyi <anthony.zyjiang@outlook.com>
2017-09-25 16:06:50 +0100

    better startup.m

---
+++ addallfolders.m
@@ -0,0 +1,51 @@
+function p = addallfolders(varargin)
+%ADDALLFOLDERS add all subfolders in the current directory to MATLAB search 
+% path.
+%
+% addallfolders(F1, F2,...) ignores the specified folders with names F1,
+% F2, etc.
+%
+% addallfolders(M, ...) add folders and subfolders which contains file M. M
+% must be specified in fullpath, for example:
+%
+% M = 'c:/root/file.txt';
+%
+% p = addallfolders(...) returns all folders that have been added.
+%
+% Copyright Zhengyi Jiang, 2017.
+
+curpath = what;
+fpath = [];
+
+if nargin > 0
+    fpath = fileparts(varargin{1});
+    % if 
+    if ~isempty(fpath)
+        cd(fpath);
+        varargin{1} = [];
+        c = what;
+    else
+        c = curpath;
+    end
+end
+
+p = genpath(c.path);
+if ~isempty(varargin)
+    p = strsplit(p,';');
+    
+    for i = 1:numel(varargin)
+        if isempty(p)
+            return
+        end
+        p = p(cellfun(@isempty, strfind(p,varargin{i})));
+    end
+    p = strjoin(p,';');
+end
+   
+% return to orginal path
+if ~isempty(fpath)
+    cd(curpath.path);
+end
+
+% add path
+addpath(p)
--- startup.m
+++ startup.m
@@ -1,18 +1,1 @@
-startupparam.ignoreddir = {'.git'};
-
-startupparam.f = mfilename('fullpath');
-[startupparam.path_, ~, ~] = fileparts(startupparam.f);
-startupparam.p = strsplit(genpath(startupparam.path_),';');
-
-for i = 1:numel(startupparam.ignoreddir)
-    if isempty(startupparam.p)
-        break;
-    end
-    startupparam.p = startupparam.p(cellfun(@isempty, strfind(startupparam.p,startupparam.ignoreddir{i})));
-end
-
-if ~isempty(startupparam.p)
-    addpath(strjoin(startupparam.p,';'))
-end
-
-clear startupparam i;
+addallfolders(mfilename('fullpath'),'.git');
