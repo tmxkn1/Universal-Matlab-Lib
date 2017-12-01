f03ce6605afb246efe78c15704b9b41399e44a76
Home Desktop <anthony.zyjiang@outlook.com>
2017-12-01 19:01:52 +0000

    - improved BSPSURFFIT

--- reverse engineering/curves and surfaces/bspline/bspsurffit.m
+++ reverse engineering/curves and surfaces/bspline/bspsurffit.m
@@ -1,16 +1,47 @@
-function [varargout]=bspsurffit(P,u,w,ncpu,ncpw,ku,kw,nnpu,nnpw)
-% P is a set of surface points in a 3D Cartesian coordinate system
-% u is parameters in u-direction;
-% w is parameters in w-direction;
-% ncp is number of control points in u direction
-% ncpw is number of control points in w direction
-% nnpu is number of fitted points in u direction
-% nnpw is number of fitted points in w direction
+function [varargout]=bspsurffit(varargin)
+% BSPSURFFIT calculates a B-spline surface based on a set of input
+% parameters.
+%
+% [BS] = BSPSURFFIT(P,U,W,CU,CW) 
+% requires inputs:
+%   * P    a set of surface points in 3D Cartesian coordinates
+%   * U    parameters in u-direction
+%   * W    parameters in w-direction
+%   * CU   Number of control points in u-direction
+%   * CW   Number of control points in w-direction
+% returns a structured varible BS:
+%   * BS.cx, BS.cy, BS.cz are CU-by-CW matrices for the x, y and z 
+%     components of the control points, respectively.
+%   * BS.knotu and BS.knotw are knot vectors in u and w directions,
+%   respectively.
+%
+% [FP, BS] = BSPSURFFIT(...,NU,NW) 
+% with two more parameters:
+%   * NU   Number of fitted points in u direction
+%   * NW   Number of fitted points in w direction
+% returns fitted surface points, FP (NU*NW-by-3), as the first argument,
+% and the same structured variable metioned above.
+%
+% by Zhengyi Jiang, The University of Manchester, 2017
 
-QX = P(:,1);
-QY = P(:,2);
-QZ = P(:,3);
-npoints = numel(QX);
+% Parse arguments
+if nargin ~= 7 && nargin ~= 9
+    error('Not enough input arguments.');
+end
+
+if nargout > 2
+    error('Too many output arguments.');
+end
+
+QX = varargin{1}(:,1);
+QY = varargin{1}(:,2);
+QZ = varargin{1}(:,3);
+u = varargin{2};
+w = varargin{3};
+ncpu = varargin{4};
+ncpw = varargin{5};
+ku = varargin{6};
+kw = varargin{7};
 
 %% U direction
 nosu = ncpu-ku+1; % number of segments
@@ -56,10 +87,14 @@ CYY = reshape(CY,ncpw,ncpu)';
 CZZ = reshape(CZ,ncpw,ncpu)';
 
 %% Generate new points and output
-if nargout == 2
+if nargout == 1 % output structured B-spline surface parameters
+    bsp.cx = CXX; bsp.cy = CYY; bsp.cz = CZZ;
+    bsp.knotu = knotVu; bsp.knotw = knotVw;
+    varargout{1} = bsp;
+else % output B-spline surface points matrix and parameters
     % generate uniform u,w for fitting.
-    ufit = linspace(0,nosu,nnpu);
-    wfit = linspace(0,nosw,nnpw);
+    ufit = linspace(0,nosu,varargin{8});
+    wfit = linspace(0,nosw,varargin{9});
 
     Ufit = bspbasis(ufit, ncpu, ku, knotVu);
     Wfit = bspbasis(wfit, ncpw, kw, knotVw);
@@ -72,9 +107,5 @@ if nargout == 2
     bsp.x = FX; bsp.y = FY; bsp.z = FZ;
     bsp.cx = CXX; bsp.cy = CYY; bsp.cz = CZZ;
     bsp.knotu = knotVu; bsp.knotw = knotVw;
-    varargout{2} = bsp;
-else
-    bsp.cx = CXX; bsp.cy = CYY; bsp.cz = CZZ;
-    bsp.knotu = knotVu; bsp.knotw = knotVw;
-    varargout{1} = bsp;
+    varargout{2} = bsp;    
 end
